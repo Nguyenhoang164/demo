9ddb590c23fee47b24911da476c6bb16ce9e563f
Nguyenhoang164 <kamito062004@gmail.com>
2024-07-23 13:06:24 +0700

    main

--- Home.java
+++ Home.java
@@ -1,5 +1,7 @@
 public class Home {
     public static void main(String[] args) {
         System.out.println("hello worldhh");
+        int number = 10;
+        System.out.println(number);
     }
 }
