2712a3b9b43a133c2b770f5c89d0c29035ec72c0
HelenaSenpai <114371018+Nguyenhoang164@users.noreply.github.com>
2024-07-23 13:09:44 +0700

    Update Home.java

--- Home.java
+++ Home.java
@@ -1,7 +1,4 @@
 public class Home {
     public static void main(String[] args) {
-        System.out.println("hello worldhh");
-        int number = 15;
-        System.out.println(number);
     }
 }
